96f6c290cf0063fa1ee466670e93ba5ddbb1b106
JP-Guillen <juan_guillen@ucne.edu.do>
2025-11-22 12:14:27 -0400

    Migracion e importe

---
+++ AgrimasterCAD/Data/Migrations/20251122153735_Actualizadoo.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgrimasterCAD.Migrations
+{
+    /// <inheritdoc />
+    public partial class Actualizadoo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SolicitudesPlano_AspNetUsers_AgrimensorId",
+                table: "SolicitudesPlano");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SolicitudesPlano_AspNetUsers_ClienteId",
+                table: "SolicitudesPlano");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SolicitudesPlano_AspNetUsers_AgrimensorId",
+                table: "SolicitudesPlano",
+                column: "AgrimensorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SolicitudesPlano_AspNetUsers_ClienteId",
+                table: "SolicitudesPlano",
+                column: "ClienteId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SolicitudesPlano_AspNetUsers_AgrimensorId",
+                table: "SolicitudesPlano");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SolicitudesPlano_AspNetUsers_ClienteId",
+                table: "SolicitudesPlano");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SolicitudesPlano_AspNetUsers_AgrimensorId",
+                table: "SolicitudesPlano",
+                column: "AgrimensorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SolicitudesPlano_AspNetUsers_ClienteId",
+                table: "SolicitudesPlano",
+                column: "ClienteId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
