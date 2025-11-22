4eb6d3b12d69cf8dd5e6818b146d6c8ac62f557d
jamesdr-10 <james_depena@ucne.edu.do>
2025-11-21 20:38:14 -0400

    bregando

---
+++ AgrimasterCAD/Data/SeedData.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace AgrimasterCAD.Data;
+
+public static class SeedData
+{
+    public static async Task InitializeAsync(IServiceProvider serviceProvider)
+    {
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+        string[] roles = { "Cliente", "Agrimensor", "Administrador" };
+
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+                await roleManager.CreateAsync(new IdentityRole(role));
+        }
+
+        string adminEmail = "admin@agrimaster.com";
+        string adminPassword = "Admin123*";
+
+        var adminUser = await userManager.FindByEmailAsync(adminEmail);
+
+        if (adminUser == null)
+        {
+            adminUser = new ApplicationUser
+            {
+                UserName = adminEmail,
+                Email = adminEmail,
+                EmailConfirmed = true
+            };
+
+            var result = await userManager.CreateAsync(adminUser, adminPassword);
+
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(adminUser, "Administrador");
+            }
+            else
+            {
+                throw new Exception("No se pudo crear el usuario administrador inicial.");
+            }
+        }
+    }
+}
